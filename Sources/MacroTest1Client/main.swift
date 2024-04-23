d0c35451e6cdc7231a7d73228351988789246177
石山岭 <2329827114@qq.com>
2024-04-23 16:30:07 +0800

    编译通过

--- Sources/MacroTest1Client/main.swift
+++ Sources/MacroTest1Client/main.swift
@@ -4,5 +4,7 @@ let a = 17
 let b = 25
 
 let (result, code) = #stringify(a + b)
-
+let (result1, code1) = #stringify(130 + 250)
+//添加一行测试代码
+print("\(result1)=====\(code1)")
 print("The value \(result) was produced by the code \"\(code)\"")
